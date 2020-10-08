54a80e665952da843f5ceeedfff8371d8f0fb1f4
Santeri Elo <me@santeri.xyz>
2020-10-08 03:16:36 +0300

    chore: update to angular10 (#1)
    * chore: update @angular/core + deps
    
    * chore: update @angular/cli + deps
    
    * chore: update the rest of @angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
